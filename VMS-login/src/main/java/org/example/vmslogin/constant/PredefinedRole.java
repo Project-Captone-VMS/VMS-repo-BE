d4c877e66a9f1e22d203a4135eb898e1db0aea33
Nhantp <103438224+Nhantp@users.noreply.github.com>
2024-10-25 23:29:42 +0700

    Delete VMS-login directory

--- VMS-login/src/main/java/org/example/vmslogin/constant/PredefinedRole.java
+++
@@ -1,8 +0,0 @@
-package org.example.vmslogin.constant;
-
-public class PredefinedRole {
-    public static final String USER_ROLE = "USER";
-    public static final String ADMIN_ROLE = "ADMIN";
-
-    private PredefinedRole() {}
-}
